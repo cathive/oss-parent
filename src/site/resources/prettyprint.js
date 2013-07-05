33ae02161bc683822483c7ca4ffaa636252f6b0f
Benjamin P. Jung <headcr4sh@gmail.com>
2013-07-05 15:02:32 +0200

    Prepare for better Github integration / deployments.

---
+++ src/site/resources/prettyprint.js
@@ -0,0 +1,12 @@
+jQuery(function($)
+{
+    /*
+     * This little snippet is used to automatically add the desired "prettyprint"
+     * class definitions to all sourcecode sections that are rendered via the
+     * doxia-docbook-simple module.
+     */
+    $("div.source>pre").addClass("prettyprint linenums:1");
+
+    
+    if (prettyPrint) prettyPrint();
+});
