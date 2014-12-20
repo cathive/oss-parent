3c5fad34873466c1bb37f0c525c0a8f54cae714f
Benjamin P. Jung <headcr4sh@gmail.com>
2014-12-20 14:17:40 +0100

    Update site deployment descriptor and JS.

--- src/site/resources/prettyprint.js
+++ src/site/resources/prettyprint.js
@@ -1,12 +1,9 @@
-jQuery(function($)
-{
+jQuery(function($) {
     /*
      * This little snippet is used to automatically add the desired "prettyprint"
      * class definitions to all sourcecode sections that are rendered via the
      * doxia-docbook-simple module.
      */
     $("div.source>pre").addClass("prettyprint linenums:1");
-
-    
-    if (prettyPrint) prettyPrint();
+    if (prettyPrint) { prettyPrint() };
 });
